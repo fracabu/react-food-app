5e55108123e76b43765c739a6c166341208e0a6d
fracabu <100199965+fracabu@users.noreply.github.com>
2023-04-15 23:51:30 +0200

    first commit

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Header.css';
+
+const Header = () => (
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1 animate__animated animate__fadeInLeft">The Key To Fine Dining</h1>
+      <p className="p__opensans animate__animated animate__fadeInRight" style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      {/* <button type="button" className="custom__button animate__animated animate__flipInX ">Explore Menu</button> */}
+      <div class="btn-container">
+        <button class="pushable animate__animated animate__flipInX">
+            <span class="shadow"></span>
+            <span class="edge"></span>
+            <span class="front">
+                3D BUTTON
+            </span>
+        </button>
+    </div>
+    </div>
+
+    <div className="app__wrapper_img animate__animated animate__fadeInRight">
+      <img src={images.welcome} alt="header_img" />
+    </div>
+  </div>
+);
+
+export default Header;
